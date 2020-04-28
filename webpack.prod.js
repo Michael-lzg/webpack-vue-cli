96137c752b73e319469cb2b229eddf602e78019e
lzg <398055960@qq.com>
2020-04-28 17:16:11 +0800

    代码压缩

--- webpack.prod.js
+++ webpack.prod.js
@@ -4,6 +4,9 @@ const common = require('./webpack.base.js');
 const PurifyCssWebpack = require('purifycss-webpack') // 引入PurifyCssWebpack插件
 const glob = require('glob') // 引入glob模块,用于扫描全部html文件中所引用的css
 const MiniCssExtractPlugin = require("mini-css-extract-plugin") //引入css分离插件
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin") // 压缩js代码
+// const ParallelUglifyPlugin = require('webpack-parallel-uglify-plugin') // 多线程压缩代码
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin") // 压缩css代码
 
 module.exports = merge(common, {
   mode: 'production',// 设置mode
@@ -32,6 +35,36 @@ module.exports = merge(common, {
     new MiniCssExtractPlugin({
       filename: "css/[name].[hash].css",
       chunkFilename: 'css/[id].[hash].css'
-    }),
-  ]
+    })
+  ],
+  optimization: {
+    // 分离chunks
+    splitChunks: {
+      chunks: 'all',
+      cacheGroups: {
+        vendor: {
+          name: "vendor",
+          test: /[\\/]node_modules[\\/]/,
+          priority: 10,
+          chunks: "initial" // 只打包初始时依赖的第三方
+        },
+      }
+    },
+    minimizer: [
+      // 压缩JS
+      new UglifyJsPlugin({
+        uglifyOptions: {
+          compress: {
+            drop_debugger: true, // 去除debugger
+            drop_console: true // 去除console.log
+          }
+        },
+        cache: true, // 开启缓存
+        parallel: true, // 平行压缩
+        sourceMap: false // set to true if you want JS source maps
+      }),
+      // 压缩css
+      new OptimizeCSSAssetsPlugin({})
+    ]
+  }
 })
